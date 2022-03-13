5f7d61b0acc0bf71d8c343461e18c13d2fd8296e
conde <developer.luis.conde@gmail.com>
2022-03-13 03:15:02 -0600

    development | se agregan properties por perfil + primer controller

---
+++ src/main/java/com/platzimarket/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzimarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class HolaMundoController {
+
+  @GetMapping("/hi")
+  public String greetings() {
+    return "Nunca pares de aprender! \uD83D\uDE80 ";
+  }
+}
