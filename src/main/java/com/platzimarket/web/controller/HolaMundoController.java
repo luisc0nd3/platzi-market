c3db0dcb9ffd23bcf262ab9b9eb59e96fccbbd32
conde <developer.luis.conde@gmail.com>
2022-03-15 20:28:18 -0600

    development | se realizan ajustes en la conexion con h2db

--- src/main/java/com/platzimarket/web/controller/HolaMundoController.java
+++ src/main/java/com/platzimarket/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platzimarket;
+package com.platzimarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
