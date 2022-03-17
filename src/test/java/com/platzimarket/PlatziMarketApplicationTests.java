8ab4c2ce8ed57368c9e38f23e534eb2138dc1df6
conde <developer.luis.conde@gmail.com>
2022-03-17 02:28:21 -0600

    development | se agrega test unitario indicado por sonar

--- src/test/java/com/platzimarket/PlatziMarketApplicationTests.java
+++ src/test/java/com/platzimarket/PlatziMarketApplicationTests.java
@@ -3,11 +3,14 @@ package com.platzimarket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class PlatziMarketApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNotNull(new PlatziMarketApplication());
 	}
 
 }
