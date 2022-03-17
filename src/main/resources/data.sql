79f5b566e94682fc946d766e7a54596647607782
conde <developer.luis.conde@gmail.com>
2022-03-17 00:34:33 -0600

    development | se agrega catalogo de metodo de pago

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,7 +10,17 @@ insert into product (id_product, name, id_category, barcode, sale_price, stock, 
 (1, 'kacang cacahuates', 1, '7500478022175', 13.0, 10, true);
 
 insert into shopping (id_shopping, id_cliente, sale_date, payment_method, comment, state) values
-(1, 1, NOW(), 1, 'Pago con tarjeta', true);
+(1, 1, NOW(), 1, 'compra de verano', true);
 
-insert into shopping_product(id_shopping, id_product, count_product, total, state) values
-(1, 1, 3, 87, true);
+insert into shopping_product(id_shopping, id_product, count_product, total, state)
+values (1, 1, 3, 87, true);
+
+CREATE TABLE cat_payment_method (
+   id_payment_method BIGINT IDENTITY PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   status BOOLEAN NOT NULL
+);
+
+insert into cat_payment_method (id_payment_method, name, status) values (1, 'Pago con tarjeta', true);
+insert into cat_payment_method (id_payment_method, name, status) values (2, 'Pago en efectivo', true);
+insert into cat_payment_method (id_payment_method, name, status) values (3, 'Pago con bitcoin', false);
